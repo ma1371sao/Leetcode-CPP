095084998e544bf745a107a4e50ee9823b92e6d6
Shijie <ma.1371sao@gmail.com>
2017-10-06 14:04:13 -0400

    Add files via upload

---
+++ LC583.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		int n1 = word1.length();
+		int n2 = word2.length();
+		if (n1 == 0) return word2.length();
+		if (n2 == 0) return word1.length();
+		vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+		for (int i = 1; i <= n1; i++)
+			for (int j = 1; j <= n2; j++) {
+				if (word1[i - 1] == word2[j - 1])
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				else
+					dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		return n1 + n2 - 2 * dp[n1][n2];
+	}
+};
