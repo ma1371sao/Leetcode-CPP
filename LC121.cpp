c86033cec35c504fa7f9b136d901ea770971219c
Shijie <ma.1371sao@gmail.com>
2017-10-20 11:31:44 -0400

    Add files via upload

--- LC121.cpp
+++ LC121.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
-		int len = 1;
-		if (prices.size() == NULL)
-			return 0;
-		vector<int>p;
-		p.push_back(0);
-		int min_i = prices[0];
-		for (int i = 1; i<prices.size(); i++)
-		{
-			if (prices[i]<prices[i - 1])
-				p.push_back(p[i - 1]);
-			else {
-				if (prices[i] - min_i>p[i - 1])
-					p.push_back(prices[i] - min_i);
-				else
-					p.push_back(p[i - 1]);
-			}
-			if (prices[i]<min_i)
-				min_i = prices[i];
+		if (prices.size() == 0) return 0;
+		int diff = 0;
+		int minNum = prices[0];
+		for (int i = 1; i < prices.size(); i++) {
+			if (prices[i] >= minNum)
+				diff = max(diff, prices[i] - minNum);
+			else
+				minNum = prices[i];
 		}
-		return *(p.end() - 1);
+		return diff;
 	}
 };
