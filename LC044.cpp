0d946f12a4caccb5ca9b5ea6b657c372a066f54c
Shijie <ma.1371sao@gmail.com>
2017-10-25 15:21:32 -0400

    Add files via upload

---
+++ LC044.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	bool isMatch(string s, string p) {
+		vector<vector<bool>> dp(s.length() + 1, vector<bool>(p.length() + 1, false));
+		dp[0][0] = true;
+		for (int j = 0; j < p.length(); j++) {
+			if (p[j] == '*' && dp[0][j])
+				dp[0][j + 1] = true;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = 0; j < p.length(); j++) {
+				if (s[i] == p[j] || p[j] == '?')
+					dp[i + 1][j + 1] = dp[i][j];
+				else {
+					if (p[j] == '*')
+						dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1]);
+				}
+			}
+		}
+		return dp[s.length()][p.length()];
+	}
+};
