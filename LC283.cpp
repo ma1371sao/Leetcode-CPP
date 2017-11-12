89f8a3122b876d1a616aa91887f0e0c6f3cc4f89
Shijie <ma.1371sao@gmail.com>
2017-11-11 19:17:47 -0500

    Delete LC283.cpp

--- LC283.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-	void moveZeroes(vector<int>& nums) {
-		int l = nums.size();
-		vector<int>::iterator it = remove(nums.begin(), nums.end(), 0);
-		for (; it<nums.end(); it++)
-			*it = 0;
-	}
-};
