53851f68f9133aa11f155213ab06c17104c8d089
Shijie <ma.1371sao@gmail.com>
2017-11-14 20:54:16 -0500

    Add files via upload

--- LC275.cpp
+++ LC275.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
 	int hIndex(vector<int>& citations) {
 		int n = citations.size();
-		for (int i = 0; i<n; i++) {
-			if (citations[i] >= n - i)
-				return n - i;
+		int left = 0;
+		int right = n - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (citations[mid] >= n - mid)
+				right = mid - 1;
+			if (citations[mid] < n - mid)
+				left = mid + 1;
 		}
-		return 0;
+		return n - left;
 	}
 };
