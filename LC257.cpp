25eaa71dcfcf1cf33a09546ab3863830f97918eb
Shijie <ma.1371sao@gmail.com>
2017-11-14 21:09:34 -0500

    Add files via upload

--- LC257.cpp
+++ LC257.cpp
@@ -9,48 +9,17 @@
 */
 class Solution {
 public:
-	vector<string> ans;
 	vector<string> binaryTreePaths(TreeNode* root) {
-		if (root == NULL)
-			return ans;
-		DFS(root, "");
-		return ans;
+		vector<string> res;
+		if (root == NULL) return res;
+		dfs(root, res, "");
+		return res;
 	}
-	void DFS(TreeNode* p, string s)
-	{
-		string s_val = Inttostring(p->val);
-		if (s == "")
-			s = s + s_val;
-		else
-			s = s + "->" + s_val;
-		if (p->left == NULL && p->right == NULL)
-		{
-			ans.push_back(s);
-			return;
-		}
-		if (p->left)
-			DFS(p->left, s);
-		if (p->right)
-			DFS(p->right, s);
-	}
-	string Inttostring(int val)
-	{
-		string s;
-		vector<int> v;
-		int flag = 0;
-		if (val<0)
-		{
-			s.push_back('-');
-			val = -val;
-		}
-		while (val)
-		{
-			int c = val % 10;
-			v.push_back(c);
-			val = val / 10;
-		}
-		for (int i = v.size() - 1; i >= 0; i--)
-			s.push_back(v[i] + 48);
-		return s;
+
+	void dfs(TreeNode* root, vector<string>& res, string curPath) {
+		curPath += to_string(root->val);
+		if (root->left) dfs(root->left, res, curPath + "->");
+		if (root->right) dfs(root->right, res, curPath + "->");
+		if (root->left == NULL && root->right == NULL) res.push_back(curPath);
 	}
 };
