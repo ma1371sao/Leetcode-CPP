b43efc6bfcf8c53ec18749260dfb7311eb881ace
Shijie <ma.1371sao@gmail.com>
2017-11-15 22:29:58 -0500

    Add files via upload

--- LC210.cpp
+++ LC210.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
 	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
 		vector<int> res;
-		int n = prerequisites.size();
-		if (numCourses == 0)   return res;
-		vector<int> innum(numCourses, 0);
-		vector<int> vis(numCourses, 0);
-		unordered_map<int, vector<int>> next;
-		for (int i = 0; i<n; i++) {
-			innum[prerequisites[i].first]++;
-			next[prerequisites[i].second].push_back(prerequisites[i].first);
+		vector<int> indegree(numCourses, 0);
+		unordered_map<int, vector<int>> arrivedPts;
+		for (auto edge : prerequisites) {
+			indegree[edge.first]++;
+			arrivedPts[edge.second].push_back(edge.first);
 		}
-		for (int i = 0; i<numCourses; i++) {
-			int flag = 0;
-			for (int j = 0; j<numCourses; j++)
-				if (innum[j] == 0 && !vis[j]) {
-					flag = 1;
-					vis[j] = 1;
-					res.push_back(j);
-					for (int k = 0; k<next[j].size(); k++)
-						innum[next[j][k]]--;
-					break;
+		while (res.size() < numCourses) {
+			int preSize = res.size();
+			for (int i = 0; i < numCourses; i++) {
+				if (indegree[i] == 0) {
+					res.push_back(i);
+					for (int point : arrivedPts[i])
+						indegree[point]--;
+					indegree[i] = -1;
 				}
-			if (!flag) {
+			}
+			if (res.size() == preSize) {
 				vector<int> v;
 				return v;
 			}
