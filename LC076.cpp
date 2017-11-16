739c2433dbc25b246b52f51e9959c6ce6c1c9df2
Shijie <ma.1371sao@gmail.com>
2017-11-16 15:20:44 -0500

    Add files via upload

--- LC076.cpp
+++ LC076.cpp
@@ -1,74 +1,32 @@
 class Solution {
 public:
 	string minWindow(string s, string t) {
-		int ns = s.length();
-		int nt = t.length();
-		if (ns == 0 || nt == 0 || ns<nt)
-			return "";
-		map<char, int> mt;
-		for (int i = 0; i<nt; i++)
-			mt[t[i]]++;
-		map<char, int> ms;
-		int pos = 0;
-		int min = ns + 1;
-		int start;
-		queue<int> q;
-		int num = nt;
-		for (pos = 0; pos <= ns - nt; pos++)
-			if (mt.find(s[pos]) != mt.end())
-				break;
-		if (pos == ns - nt + 1)
-			return "";
-		ms[s[pos]]++;
-		num--;
-		q.push(pos);
-		start = pos;
-		int l;
-		pos++;
-		while (!q.empty())
-		{
-			l = pos - q.front();
-			while (pos<ns && num>0 && l<min)
-			{
-				if (mt.find(s[pos]) != mt.end())
-				{
-					ms[s[pos]]++;
-					if (ms[s[pos]] <= mt[s[pos]])
-						num--;
-					q.push(pos);
+		if (s.length() == 0 || t.length() == 0) return "";
+		unordered_map<char, int> charNum;
+		for (char c : t)
+			charNum[c]++;
+		queue<int> pos;
+		int length = 0;
+		int minLen = INT_MAX;
+		string res = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (t.find(s[i]) != string::npos) {
+				charNum[s[i]]--;
+				if (charNum[s[i]] >= 0)
+					length++;
+				pos.push(i);
+			}
+			while (length == t.length()) {
+				if (minLen > i - pos.front() + 1) {
+					minLen = i - pos.front() + 1;
+					res = s.substr(pos.front(), minLen);
 				}
-				pos++;
-				l++;
-			}
-			if (num == 0)
-			{
-				if (l<min)
-				{
-					min = l;
-					start = q.front();
-				}
-				ms[s[q.front()]]--;
-				if (ms[s[q.front()]]<mt[s[q.front()]])
-					num++;
-				q.pop();
-			}
-			else if (l == min)
-			{
-				ms[s[q.front()]]--;
-				if (ms[s[q.front()]]<mt[s[q.front()]])
-					num++;
-				q.pop();
-			}
-			else
-			{
-				break;
+				charNum[s[pos.front()]]++;
+				if (charNum[s[pos.front()]] > 0)
+					length--;
+				pos.pop();
 			}
 		}
-		if (min == ns + 1)
-			return "";
-		string ans;
-		for (int i = start; i<start + min; i++)
-			ans.push_back(s[i]);
-		return ans;
+		return res;
 	}
 };
