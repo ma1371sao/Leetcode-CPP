407e775129f2b63b4b4edf013cb1e08db3639b5b
Shijie Ma <ma.1371sao@gmail.com>
2017-11-26 22:28:33 -0500

    Add files via upload

--- LC057.cpp
+++ LC057.cpp
@@ -10,60 +10,39 @@
 class Solution {
 public:
 	vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
-		int n = intervals.size();
-		vector<Interval> ans;
-		if (n == 0)
-		{
-			ans.push_back(newInterval);
-			return ans;
+		vector<Interval> res;
+		if (intervals.size() == 0) {
+			res.push_back(newInterval);
+			return res;
 		}
-		for (int k = 0; k<n;)
-		{
-			if (intervals[k].end<newInterval.start)
-			{
-				ans.push_back(intervals[k]);
-				if (k == n - 1)
-					ans.push_back(newInterval);
-				else
-				{
-					if (intervals[k + 1].start>newInterval.end)
-					{
-						ans.push_back(newInterval);
-						for (int i = k + 1; i<n; i++)
-							ans.push_back(intervals[i]);
-						break;
-					}
-				}
-				k++;
+		int start = newInterval.start;
+		int end = newInterval.end;
+		bool hasMerged = false;
+		if (newInterval.end < intervals[0].start) {
+			res.push_back(newInterval);
+			hasMerged = true;
+		}
+		for (int i = 0; i < intervals.size(); i++) {
+			if (intervals[i].end < start) {
+				res.push_back(intervals[i]);
 			}
-			else
-			{
-				int start = intervals[k].start;
-				if (start>newInterval.start)
-					start = newInterval.start;
-				int end = newInterval.end;
-				if (intervals[k].start>newInterval.end)
-					end = newInterval.end;
-				else if (end <= intervals[k].end)
-				{
-					end = intervals[k].end;
-					k++;
+			else if (intervals[i].start > end) {
+				if (!hasMerged) {
+					hasMerged = true;
+					Interval merged(start, end);
+					res.push_back(merged);
 				}
-				else {
-					k++;
-					for (; k<n && end >= intervals[k].start; k++)
-					{
-						if (end<intervals[k].end)
-							end = intervals[k].end;
-					}
-				}
-				Interval val(start, end);
-				ans.push_back(val);
-				for (; k<n; k++)
-					ans.push_back(intervals[k]);
-				break;
+				res.push_back(intervals[i]);
+			}
+			else {
+				start = min(start, intervals[i].start);
+				end = max(end, intervals[i].end);
 			}
 		}
-		return ans;
+		if (!hasMerged) {
+			Interval merged(start, end);
+			res.push_back(merged);
+		}
+		return res;
 	}
 };
