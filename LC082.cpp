91a0944aae06b129c1ed37dcdedfaf24f4c3b840
Shijie Ma <ma.1371sao@gmail.com>
2018-02-15 11:38:45 -0500

    Add files via upload

--- LC082.cpp
+++ LC082.cpp
@@ -1,37 +1,34 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* deleteDuplicates(ListNode* head) {
-		if (head == NULL)  return head;
-		unordered_map<int, ListNode*> preVal;
-		ListNode* start = new ListNode(0);
-		start->next = head;
-		ListNode* p = head->next;
-		preVal[head->val] = start;
-		ListNode* pre = head;
-		while (p) {
-			if (preVal.find(p->val) == preVal.end()) {
-				preVal[p->val] = pre;
-				pre = p;
-				p = p->next;
-			}
-			else {
-				pre->next = p->next;
-				if (preVal[p->val] != NULL) {
-					if (preVal[p->val]->next == pre)   pre = preVal[p->val];
-					preVal[p->val]->next = preVal[p->val]->next->next;
-					preVal[p->val] = NULL;
-				}
-				p = p->next;
-			}
-		}
-		return start->next;
-	}
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* preHead = new ListNode(0);
+        preHead->next = head;
+        ListNode* pre = preHead;
+        ListNode* p = head;
+        while (p) {
+            bool doRemove = false;
+            while (p->next && p->val == p->next->val) {
+                p = p->next;
+                doRemove = true;
+            }
+            if (doRemove) {
+                pre->next = p->next;
+                p = p->next;
+            }
+            else {
+                pre = p;
+                p = p->next;
+            }
+        }
+        return preHead->next;
+    }
 };
