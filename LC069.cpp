5c3db0b19e4cb256f062442bee21602f7378ba8e
Shijie Ma <ma.1371sao@gmail.com>
2018-02-17 17:30:03 -0500

    Add files via upload

--- LC069.cpp
+++ LC069.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-	int mySqrt(int x) {
-		if (x == 0)
-			return 0;
-		long long left = 1;
-		long long right = x;
-		long long mid;
-		while (left <= right)
-		{
-			mid = left + (right - left) / 2;
-			if (mid*mid == x)
-				return mid;
-			if (mid*mid<x)
-			{
-				left = mid + 1;
-			}
-			if (mid*mid>x)
-			{
-				right = mid - 1;
-			}
-		}
-		return right;
-	}
+    int mySqrt(int x) {
+        if (x < 1) return 0;
+        long long left = 1;
+        long long right = x;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            if (mid * mid == x) return mid;
+            else if (mid * mid > x)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return right;
+    }
 };
