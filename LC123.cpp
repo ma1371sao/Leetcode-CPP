32f221fd41efe2a9cfe881c5b302d5bf6291a93c
Shijie Ma <ma.1371sao@gmail.com>
2018-02-19 17:54:47 -0500

    Add files via upload

--- LC123.cpp
+++ LC123.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-	int maxProfit(vector<int>& prices) {
-		if (prices.size() <= 1) return 0;
-		vector<vector<int>> dp(3, vector<int>(prices.size(), 0));
-		for (int i = 1; i <= 2; i++) {
-			int maxVal = dp[i - 1][0] - prices[0];
-			for (int j = 1; j < prices.size(); j++) {
-				dp[i][j] = max(dp[i][j - 1], prices[j] + maxVal);
-				maxVal = max(maxVal, dp[i - 1][j] - prices[j]);
-			}
-		}
-		return dp[2][prices.size() - 1];
-	}
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) return 0;
+        vector<vector<int>> dp(3, vector<int>(prices.size(), 0));
+        for (int k = 1; k <= 2; k++) {
+            int profitAtBuy = dp[k - 1][0] - prices[0];
+            for (int i = 1; i < prices.size(); i++) {
+                dp[k][i] = max(dp[k][i - 1], prices[i] + profitAtBuy);
+                profitAtBuy = max(profitAtBuy, dp[k - 1][i - 1] - prices[i]);
+            }
+        }
+        return dp[2][prices.size() - 1];
+    }
 };
