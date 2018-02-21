718f4e4a17223998fd80a704cbb939496d2d2027
Shijie Ma <ma.1371sao@gmail.com>
2018-02-21 17:15:06 -0500

    Add files via upload

--- LC132.cpp
+++ LC132.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-	int minCut(string s) {
-		vector<int> dp(s.length() + 1, 0);
-		vector<vector<bool>> pal(s.length(), vector<bool>(s.length(), false));
-		for (int i = 1; i < s.length(); i++) {
-			dp[i + 1] = dp[i] + 1;
-			for (int j = 0; j < i; j++) {
-				if (s[j] == s[i] && (i - j <= 2 || pal[j + 1][i - 1])) {
-					pal[j][i] = true;
-					if (j == 0)
-						dp[i + 1] = 0;
-					else
-						dp[i + 1] = min(dp[i + 1], dp[j] + 1);
-				}
-			}
-		}
-		return dp[s.length()];
-	}
+    int minCut(string s) {
+        vector<vector<bool>> isPal(s.length(), vector<bool>(s.length(), false));
+        vector<int> minCut(s.length() + 1, 0);
+        isPal[0][0] = true;
+        for (int i = 1; i < s.length(); i++) {
+            isPal[i][i] = true;
+            minCut[i + 1] = minCut[i] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (s[j] == s[i] && (i < j + 2 || isPal[j + 1][i - 1])) {
+                    isPal[j][i] = true;
+                    if (j == 0)
+                        minCut[i + 1] = 0;
+                    else
+                        minCut[i + 1] = min(minCut[i + 1], minCut[j] + 1);
+                }
+            }
+        }
+        return minCut[s.length()];
+    }
 };
