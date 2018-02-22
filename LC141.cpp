c7beaa739b0eb89afac022b50babf4642b4db4a5
Shijie Ma <ma.1371sao@gmail.com>
2018-02-22 16:53:14 -0500

    Add files via upload

--- LC141.cpp
+++ LC141.cpp
@@ -1,23 +1,21 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	bool hasCycle(ListNode *head) {
-		if (!head || head->next == NULL)   return false;
-		ListNode* fast = head;
-		ListNode* slow = head;
-		while (fast->next != NULL && fast->next->next != NULL) {
-			if (fast->next->next == slow->next)
-				return true;
-			fast = fast->next->next;
-			slow = slow->next;
-		}
-		return false;
-	}
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
 };
