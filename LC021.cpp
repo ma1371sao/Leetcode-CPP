682b6d569ddc5a5d74b72148191a035fa8f4c0d7
Shijie Ma <ma.1371sao@gmail.com>
2018-02-22 16:18:43 -0500

    Add files via upload

--- LC021.cpp
+++ LC021.cpp
@@ -1,59 +1,42 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		ListNode* head = new ListNode(0);
-		if (l1 == NULL && l2 == NULL)
-			return NULL;
-		ListNode* p = head;
-		int flag = 1;
-		while (l1 && l2)
-		{
-			if (flag)
-				flag = 0;
-			else
-			{
-				p->next = new ListNode(0);
-				p = p->next;
-			}
-			if (l1->val <= l2->val)
-			{
-				p->val = l1->val;
-				l1 = l1->next;
-			}
-			else
-			{
-				p->val = l2->val;
-				l2 = l2->next;
-			}
-		}
-		if (l1)
-		{
-			if (flag)
-			{
-				p->val = l1->val;
-				p->next = l1->next;
-			}
-			else
-				p->next = l1;
-		}
-		if (l2)
-		{
-			if (flag)
-			{
-				p->val = l2->val;
-				p->next = l2->next;
-			}
-			else
-				p->next = l2;
-		}
-		return head;
-	}
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL)    return l2;
+        if(l2==NULL)    return l1;
+        ListNode* p=l1;
+        ListNode* q=l2;
+        ListNode* start=new ListNode(0);
+        ListNode* k=start;
+        while(p || q){
+            if(p && q){
+                if(p->val<=q->val){
+                    k->next=p;
+                    p=p->next;
+                    k=k->next;
+                }
+                else{
+                    k->next=q;
+                    q=q->next;
+                    k=k->next;
+                }
+            }
+            else if(p){
+                k->next=p;
+                break;
+            }
+            else{
+                k->next=q;
+                break;
+            }
+        }
+        return start->next;
+    }
 };
