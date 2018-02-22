646bd47edb89990c3794035aa3aa95f394ca6902
Shijie Ma <ma.1371sao@gmail.com>
2018-02-22 16:15:54 -0500

    Add files via upload

--- LC139.cpp
+++ LC139.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-	bool wordBreak(string s, unordered_set<string>& wordDict) {
-		int n = s.length();
-		if (n == 0)
-			return false;
-		int dp[10000];
-		memset(dp, 0, sizeof(dp));
-		for (int i = 0; i<n; i++)
-			if (wordDict.find(s.substr(0, i + 1)) != wordDict.end())
-				dp[i] = 1;
-		for (int i = 1; i<n; i++)
-		{
-			if (dp[i] == 0)
-			{
-				for (int j = i - 1; j >= 0; j--)
-				{
-					if (dp[j])
-					{
-						if (wordDict.find(s.substr(j + 1, i - j)) != wordDict.end())
-						{
-							dp[i] = 1;
-							break;
-						}
-					}
-				}
-			}
-		}
-		if (dp[n - 1])
-			return true;
-		return false;
-	}
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dictSet(wordDict.begin(), wordDict.end());
+        vector<vector<bool>> canSegment(s.length(), vector<bool>(s.length(), false));
+        for (int i = 0; i < s.length(); i++) {
+            if (dictSet.find(s.substr(0, i + 1)) != dictSet.end())
+                canSegment[0][i] = true;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            if (!canSegment[0][i]) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (canSegment[0][j] && dictSet.find(s.substr(j + 1, i - j)) != dictSet.end()) {
+                        canSegment[0][i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return canSegment[0][s.length() - 1];
+    }
 };
