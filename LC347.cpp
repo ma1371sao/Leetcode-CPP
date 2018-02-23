9abdd14fe3d0be0393ca9901502cdd54ecda5bb1
Shijie Ma <ma.1371sao@gmail.com>
2018-02-22 22:20:54 -0500

    Add files via upload

---
+++ LC347.cpp
@@ -0,0 +1,23 @@
+struct cmp {
+    bool operator() (const pair<int, int>& p1, const pair<int, int>& p2) {
+        return p1.second < p2.second;
+    }
+};
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+        vector<int> res;
+        unordered_map<int, int> val_n;
+        for (int num : nums)
+            val_n[num]++;
+        for (auto it : val_n) {
+            pq.push(make_pair(it.first, it.second));
+        }
+        for (int i = 0; i < k; i++) {
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        return res;
+    }
+};
