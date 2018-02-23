b367e2ebd1d533ba362bb9fe4dad4682adc42b1c
Shijie Ma <ma.1371sao@gmail.com>
2018-02-23 12:34:36 -0500

    Add files via upload

--- LC337.cpp
+++ LC337.cpp
@@ -1,34 +1,30 @@
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	int rob(TreeNode* root) {
-		if (!root)   return 0;
-		vector<int> res = dfs(root);
-		return res[1];
-	}
-	vector<int> dfs(TreeNode* root) {
-		if (!root) {
-			vector<int> v;
-			v.push_back(0);
-			v.push_back(0);
-			return v;
-		}
-		vector<int> vleft = dfs(root->left);
-		vector<int> vright = dfs(root->right);
-		vector<int> v(2);
-		v[0] = vleft[1] + vright[1];
-		if (v[0]>root->val + vleft[0] + vright[0])
-			v[1] = v[0];
-		else
-			v[1] = root->val + vleft[0] + vright[0];
-		return v;
-	}
+    int rob(TreeNode* root) {
+        if (root == NULL) return 0;
+        vector<int> maxRob = dfs(root);
+        return maxRob[1];
+    }
+    
+    vector<int> dfs(TreeNode* root) {
+        if (root == NULL) {
+            vector<int> maxRob(2, 0);
+            return maxRob;
+        }
+        vector<int> maxRob_l = dfs(root->left);
+        vector<int> maxRob_r = dfs(root->right);
+        vector<int> maxRob(2);
+        maxRob[0] = maxRob_l[1] + maxRob_r[1];
+        maxRob[1] = max(root->val + maxRob_l[0] + maxRob_r[0], maxRob[0]);
+        return maxRob;
+    }
 };
