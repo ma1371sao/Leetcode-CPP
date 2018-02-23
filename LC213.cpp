4601966589767b78b87fb2d90b09fa52ff1ca445
Shijie Ma <ma.1371sao@gmail.com>
2018-02-23 12:12:48 -0500

    Add files via upload

--- LC213.cpp
+++ LC213.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-	int rob(vector<int>& nums) {
-		if (nums.size() == 0)  return 0;
-		if (nums.size() == 1)  return nums[0];
-		vector<int> dp(nums.size(), 0);
-		dp[0] = nums[0];
-		if (nums[1] >= nums[0])    dp[1] = nums[1];
-		else dp[1] = nums[0];
-		for (int i = 2; i<nums.size() - 1; i++) {
-			if (nums[i] + dp[i - 2] >= dp[i - 1])
-				dp[i] = nums[i] + dp[i - 2];
-			else
-				dp[i] = dp[i - 1];
-		}
-		int res = dp[nums.size() - 2];
-		dp[0] = 0;
-		dp[1] = nums[1];
-		for (int i = 2; i<nums.size(); i++) {
-			if (nums[i] + dp[i - 2] >= dp[i - 1])
-				dp[i] = nums[i] + dp[i - 2];
-			else
-				dp[i] = dp[i - 1];
-		}
-		if (res >= dp[nums.size() - 1])
-			return res;
-		else
-			return dp[nums.size() - 1];
-	}
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        vector<int> rob1(nums.size() + 1, 0);
+        vector<int> rob2(nums.size() + 1, 0);
+        rob1[1] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            rob1[i + 1] = max(rob1[i], nums[i] + rob1[i - 1]);
+            rob2[i + 1] = max(rob2[i], nums[i] + rob2[i - 1]);
+        }
+        return max(rob1[nums.size() - 1], rob2[nums.size()]);
+    }
 };
