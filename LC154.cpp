263fada3e75d4da9968494d9af50e03481a971e9
Shijie Ma <ma.1371sao@gmail.com>
2018-02-27 14:06:03 -0500

    Add files via upload

--- LC154.cpp
+++ LC154.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
-	int findMin(vector<int>& nums) {
-		for (int i = 0; i < nums.size() - 1; i++)
-			if (nums[i] > nums[i + 1])
-				return nums[i + 1];
-		return nums[0];
-	}
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[left])
+                right = mid;
+            else if (nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right--;
+        }
+        return nums[left];
+    }
 };
