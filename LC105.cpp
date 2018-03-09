7f0bbde48fe7f032afc0c49a53e7cc0ad8547247
Shijie Ma <ma.1371sao@gmail.com>
2018-03-09 14:43:45 -0500

    Add files via upload

--- LC105.cpp
+++ LC105.cpp
@@ -1,49 +1,47 @@
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-		int n = preorder.size();
-		if (inorder.size() != n || n == 0)   return NULL;
-		TreeNode* root = new TreeNode(preorder[0]);
-		unordered_map<int, int> hashin;
-		for (int i = 0; i<n; i++)
-			hashin[inorder[i]] = i;
-		vector<bool> vis(n, false);
-		vis[hashin[preorder[0]]] = true;
-		int pos = 0;
-		if (hashin[preorder[0]]>0)
-			pos = dfs(preorder, inorder, hashin, pos + 1, root, 0, vis);
-		if (hashin[preorder[0]]<n - 1)
-			dfs(preorder, inorder, hashin, pos + 1, root, 1, vis);
-		return root;
-	}
-	int dfs(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& hashin, int pos, TreeNode* root, int flag, vector<bool>& vis) {
-		int cur = pos;
-		if (flag) {
-			root->right = new TreeNode(preorder[cur]);
-			vis[hashin[preorder[cur]]] = true;
-			if (hashin[preorder[cur]] - 1 >= 0 && !vis[hashin[preorder[cur]] - 1])
-				pos = dfs(preorder, inorder, hashin, pos + 1, root->right, 0, vis);
-			if (hashin[preorder[cur]] + 1<preorder.size() && !vis[hashin[preorder[cur]] + 1])
-				pos = dfs(preorder, inorder, hashin, pos + 1, root->right, 1, vis);
-			return pos;
-		}
-		else {
-			root->left = new TreeNode(preorder[cur]);
-			vis[hashin[preorder[cur]]] = true;
-			if (hashin[preorder[cur]] - 1 >= 0 && !vis[hashin[preorder[cur]] - 1])
-				pos = dfs(preorder, inorder, hashin, pos + 1, root->left, 0, vis);
-			if (hashin[preorder[cur]] + 1<preorder.size() && !vis[hashin[preorder[cur]] + 1])
-				pos = dfs(preorder, inorder, hashin, pos + 1, root->left, 1, vis);
-			return pos;
-		}
-	}
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        if (n == 0) return NULL;
+        TreeNode* root = new TreeNode(preorder[0]);
+        unordered_map<int, int> val_inPos;
+        for (int i = 0; i < n; i++)
+            val_inPos[inorder[i]] = i;
+        int pos = 0;
+        if (val_inPos[preorder[0]] > 0)
+            pos = construct(root, val_inPos, preorder, inorder, pos + 1, 0, val_inPos[preorder[pos]]);
+        if (val_inPos[preorder[0]] < preorder.size() - 1)
+            construct(root, val_inPos, preorder, inorder, pos + 1, 1, n);
+        return root;
+    }
+    
+    int construct(TreeNode* root, unordered_map<int, int>& val_inPos, vector<int>& preorder, vector<int>& inorder, int pos,
+                  int flag, int rightLimit) {
+        if (flag == 0) {
+            root->left = new TreeNode(preorder[pos]);
+            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < val_inPos[preorder[pos]])
+                pos = construct(root->left, val_inPos, preorder, inorder, pos + 1, 0, val_inPos[preorder[pos]]);
+            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < rightLimit)
+                pos = construct(root->left, val_inPos, preorder, inorder, pos + 1, 1, rightLimit);
+            return pos;
+        }
+        else {
+            cout << pos << endl;
+            root->right = new TreeNode(preorder[pos]);
+            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < val_inPos[preorder[pos]])
+                pos = construct(root->right, val_inPos, preorder, inorder, pos + 1, 0, val_inPos[preorder[pos]]);
+            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < rightLimit)
+                pos = construct(root->right, val_inPos, preorder, inorder, pos + 1, 1, rightLimit);
+            return pos;
+        }
+    }
 };
