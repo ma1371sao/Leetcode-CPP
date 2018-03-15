afb1798842aab78c2b8bc664ac7ba000aa2cc55b
Shijie Ma <ma.1371sao@gmail.com>
2018-03-14 20:02:58 -0400

    Add files via upload

---
+++ LC209.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int minLen = INT_MAX;
+        int sum = 0;
+        int start = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                minLen = min(minLen, i - start + 1);
+                sum -= nums[start++];
+            }
+        }
+        return (minLen == INT_MAX ? 0 : minLen);
+    }
+};
