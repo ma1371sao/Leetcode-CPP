7f0549818fc1af595fa48d723d27d9b84ba68c15
Shijie Ma <ma.1371sao@gmail.com>
2018-03-16 09:57:57 -0400

    Add files via upload

--- LC235.cpp
+++ LC235.cpp
@@ -1,34 +1,20 @@
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-		TreeNode *ans;
-		if (p == q)
-			return p;
-		if (p->val<q->val)
-			ans = BST(root, p, q);
-		else
-			ans = BST(root, q, p);
-		return ans;
-	}
-	TreeNode* BST(TreeNode* fa, TreeNode* l, TreeNode* r)
-	{
-		if (fa == l || fa == r)
-			return fa;
-		if (fa->val > l->val && fa->val < r->val)
-			return fa;
-		if (fa->val > r->val)
-			return BST(fa->left, l, r);
-		if (fa->val < l->val)
-			return BST(fa->right, l, r);
-		return fa;
-	}
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) return NULL;
+        if (p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
 };
