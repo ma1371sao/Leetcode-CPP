e72471cb57bdff8a7521afdb1977305347f47055
Shijie Ma <ma.1371sao@gmail.com>
2018-03-19 14:40:41 -0400

    Add files via upload

--- LC274.cpp
+++ LC274.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-	int hIndex(vector<int>& citations) {
-		sort(citations.begin(), citations.end());
-		int h = citations.size();
-		for (int ct : citations) {
-			if (ct >= h)
-				return h;
-			h--;
-		}
-		return h;
-	}
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        if (n == 0) return 0;
+        unordered_map<int, int> h_n;
+        for (int ct : citations) {
+            if (ct > n) h_n[n]++;
+            else h_n[ct]++;
+        }
+        int last = 0;
+        for (int i = n; i >= 0; i--) {
+            h_n[i] += last;
+            if (h_n[i] >= i) return i;
+            last = h_n[i];
+        }
+        return -1;
+    }
 };
