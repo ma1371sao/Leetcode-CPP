25970e1d9fe7a2bec4a1650fdbfeff824ae1bf2f
Shijie Ma <ma.1371sao@gmail.com>
2018-03-24 17:18:02 -0400

    Add files via upload

--- LC287.cpp
+++ LC287.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-	int findDuplicate(vector<int>& nums) {
-		for (int i = 0; i < nums.size(); i++)
-			for (int j = i + 1; j<nums.size(); j++)
-				if (nums[i] == nums[j])
-					return nums[i];
-	}
+    int findDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int ori = abs(nums[i]);
+            if (nums[ori] < 0) return ori;
+            nums[ori] = -nums[ori];
+        }
+        return 0;
+    }
 };
