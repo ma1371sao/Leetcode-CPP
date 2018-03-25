7e50095387925bc271bffcb3c73e6e5d1533a0c9
Shijie Ma <ma.1371sao@gmail.com>
2018-03-24 21:28:49 -0400

    Add files via upload

--- LC304.cpp
+++ LC304.cpp
@@ -1,35 +1,28 @@
 class NumMatrix {
 public:
-	vector<vector<int>> m;
-	vector<vector<int>> sum;
-	NumMatrix(vector<vector<int>> &matrix) {
-		m = matrix;
-		int row = m.size();
-		if (row != 0) {
-			int col = m[0].size();
-			vector<vector<int>> v(row + 1, vector<int>(col + 1, 0));
-			sum = v;
-			int val = 0;
-			for (int r = 0; r<row; r++)
-				for (int c = 0; c<col; c++) {
-					val += m[r][c];
-					sum[r + 1][c + 1] = val;
-				}
-			for (int r = 2; r<row + 1; r++)
-				sum[r][0] = sum[r - 1][col];
-		}
-	}
-
-	int sumRegion(int row1, int col1, int row2, int col2) {
-		int res = 0;
-		for (int r = row1; r <= row2; r++)
-			res += sum[r + 1][col2 + 1] - sum[r + 1][col1];
-		return res;
-	}
+    vector<vector<int>> sum;
+    NumMatrix(vector<vector<int>> matrix) {
+        int r = matrix.size();
+        if (r == 0) return;
+        int c = matrix[0].size();
+        sum = vector<vector<int>>(r, vector<int>(c + 1, 0));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++)
+                sum[i][j + 1] = sum[i][j] + matrix[i][j];
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sumRec = 0;
+        for (int i = row1; i <= row2; i++) {
+            sumRec += sum[i][col2 + 1] - sum[i][col1];
+        }
+        return sumRec;
+    }
 };
 
-
-// Your NumMatrix object will be instantiated and called as such:
-// NumMatrix numMatrix(matrix);
-// numMatrix.sumRegion(0, 1, 2, 3);
-// numMatrix.sumRegion(1, 2, 3, 4);
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
