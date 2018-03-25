7fb24050c7e250d696ea42189ba01edf0f840d87
Shijie Ma <ma.1371sao@gmail.com>
2018-03-25 14:33:53 -0400

    Add files via upload

--- LC310.cpp
+++ LC310.cpp
@@ -1,56 +1,48 @@
+//O(E + n)
 class Solution {
 public:
-	vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-		vector<int> res;
-		unordered_map<int, int> indegree;
-		unordered_map<int, vector<int>> nxt;
-		if (n == 1) {
-			res.push_back(0);
-			return res;
-		}
-		if (n == 2) {
-			res.push_back(0);
-			res.push_back(1);
-			return res;
-		}
-		for (int i = 0; i<edges.size(); i++) {
-			indegree[edges[i].first]++;
-			indegree[edges[i].second]++;
-			nxt[edges[i].first].push_back(edges[i].second);
-			nxt[edges[i].second].push_back(edges[i].first);
-		}
-		queue<int> v;
-		vector<int> vis(n, 0);
-		int num = n;
-		for (int i = 0; i<n; i++)
-			if (indegree[i] == 1) {
-				v.push(i);
-				vis[i] = 1;
-				num--;
-			}
-		while (!v.empty()) {
-			if ((v.size() == 2 || v.size() == 1) && num == 0) {
-				while (!v.empty()) {
-					res.push_back(v.front());
-					v.pop();
-				}
-				return res;
-			}
-			int number = v.size();
-			for (int i = 0; i<number; i++) {
-				int node = v.front();
-				v.pop();
-				for (int j = 0; j<nxt[node].size(); j++)
-					if (!vis[nxt[node][j]]) {
-						indegree[nxt[node][j]]--;
-						if (indegree[nxt[node][j]] == 1) {
-							v.push(nxt[node][j]);
-							vis[nxt[node][j]] = 1;
-							num--;
-						}
-					}
-			}
-		}
-		return res;
-	}
+    struct Node {
+        unordered_set<int> neighbors;
+        bool isLeaf() {return neighbors.size() == 1;}
+    };
+    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+        vector<int> res;
+        if (n == 0) return res;
+        if (n == 1) {
+            res.push_back(0);
+            return res;
+        }
+        if (n == 2) {
+            res.push_back(0);
+            res.push_back(1);
+            return res;
+        }
+        vector<Node> nodes(n);
+        for (auto e : edges) {
+            nodes[e.first].neighbors.insert(e.second);
+            nodes[e.second].neighbors.insert(e.first);
+        }
+        queue<int> leaves;
+        for (int i = 0; i < n; i++)
+            if (nodes[i].isLeaf())
+                leaves.push(i);
+        while (leaves.size() < n) {
+            int num_l = leaves.size();
+            n -= num_l;
+            for (int i = 0; i < num_l; i++) {
+                int leaf = leaves.front();
+                leaves.pop();
+                for (auto i : nodes[leaf].neighbors) {
+                    nodes[i].neighbors.erase(leaf);
+                    if (nodes[i].isLeaf())
+                        leaves.push(i);
+                }
+            }
+        }
+        while (!leaves.empty()) {
+            res.push_back(leaves.front());
+            leaves.pop();
+        }
+        return res;
+    }
 };
