6167ddc918495b0baed7c8cdb76b9959e0cc4fe6
Shijie Ma <ma.1371sao@gmail.com>
2018-03-27 22:41:12 -0400

    Add files via upload
    O(n)

--- LC451.cpp
+++ LC451.cpp
@@ -1,33 +1,18 @@
-struct character {
-	char ch;
-	int num;
-	character(char c, int n) : ch(c), num(n) {}
-};
-bool cmp(const character a, const character b) {
-	return a.num > b.num;
-}
 class Solution {
 public:
-	string frequencySort(string s) {
-		unordered_map<char, int> charIndex;
-		vector<character> characters;
-		for (int i = 0; i < s.length(); i++) {
-			if (charIndex.find(s[i]) == charIndex.end()) {
-				characters.push_back(character(s[i], 1));
-				charIndex[s[i]] = characters.size() - 1;
-			}
-			else {
-				characters[charIndex[s[i]]].num++;
-			}
-		}
-		sort(characters.begin(), characters.end(), cmp);
-		string res;
-		for (int i = 0; i < characters.size(); i++) {
-			while (characters[i].num > 0) {
-				res.push_back(characters[i].ch);
-				characters[i].num--;
-			}
-		}
-		return res;
-	}
+    string frequencySort(string s) {
+        vector<string> bucket(s.length() + 1, "");
+        unordered_map<char, int> char_num;
+        for (auto c: s) char_num[c]++;
+        for (auto it : char_num) {
+            char c = it.first;
+            int num = it.second;
+            string s(num, c);
+            bucket[num] += s;
+        }
+        string res;
+        for (int i = s.length(); i > 0; i--)
+            res += bucket[i];
+        return res;
+    }
 };
