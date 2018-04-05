c5da12055263a69732505ac429ba3e8597415aa9
Shijie Ma <ma.1371sao@gmail.com>
2018-04-05 12:27:37 -0400

    Add files via upload

--- LC338.cpp
+++ LC338.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-	vector<int> countBits(int num) {
-		vector<int> res;
-		if (num<0)   return res;
-		res.push_back(0);
-		int base = 1;
-		for (int i = 1; i <= num; i++) {
-			while (base <= i)
-				base *= 2;
-			base /= 2;
-			res.push_back(1 + res[i - base]);
-		}
-		return res;
-	}
+    vector<int> countBits(int num) {
+        vector<int> bits(num + 1, 0);
+        for (int i = 1; i <= num; i++)
+            bits[i] = bits[i / 2] + (i & 1); // or bits[i] = bits[i / 2] + i % 2;
+        return bits;
+    }
 };
