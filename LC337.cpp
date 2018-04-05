ee52e31cdca8e9242280cc8ef453901c748b7411
Shijie Ma <ma.1371sao@gmail.com>
2018-04-05 11:58:57 -0400

    Add files via upload

--- LC337.cpp
+++ LC337.cpp
@@ -10,20 +10,17 @@
 class Solution {
 public:
     int rob(TreeNode* root) {
-        vector<int> maxRob = dfs(root);
-        return max(maxRob[0], maxRob[1]);
+        vector<int> robMoney = dfs(root);
+        return robMoney[1];
     }
     
     vector<int> dfs(TreeNode* root) {
-        if (root == NULL) {
-            vector<int> maxRob(2, 0);
-            return maxRob;
-        }
-        vector<int> maxRob_l = dfs(root->left);
-        vector<int> maxRob_r = dfs(root->right);
-        vector<int> maxRob(2); //0: not include root; 1: include
-        maxRob[0] = maxRob_l[1] + maxRob_r[1];
-        maxRob[1] = max(root->val + maxRob_l[0] + maxRob_r[0], maxRob[0]);
-        return maxRob;
+        vector<int> robMoney(2, 0);
+        if (root == NULL) return robMoney;
+        vector<int> left_robMoney = dfs(root->left);
+        vector<int> right_robMoney = dfs(root->right);
+        robMoney[0] = left_robMoney[1] + right_robMoney[1];
+        robMoney[1] = max(left_robMoney[0] + right_robMoney[0] + root->val, robMoney[0]);
+        return robMoney;
     }
 };
