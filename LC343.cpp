a86d1ba7dfe4587269622eabaef0e42af6adac5f
Shijie Ma <ma.1371sao@gmail.com>
2018-04-05 20:22:32 -0400

    Add files via upload

--- LC343.cpp
+++ LC343.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-	int integerBreak(int n) {
-		if (n == 2)    return 1;
-		if (n == 3)    return 2;
-		if (n == 4)    return 4;
-		int res = 1;
-		while (n>4) {
-			res *= 3;
-			n -= 3;
-		}
-		res *= n;
-		return res;
-	}
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        if (n == 4) return 4;
+        int res = 1;
+        while (n > 4) {
+            res *= 3;
+            n -= 3;
+        }
+        return res * n;
+    }
 };
