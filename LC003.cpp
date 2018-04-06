b09fdda909dfe29f2049d9ee7aa14c43f1daf487
Shijie Ma <ma.1371sao@gmail.com>
2018-04-06 15:07:40 -0400

    Add files via upload

--- LC003.cpp
+++ LC003.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int res = 0;
-        unordered_map<char, int> ch_pos;
+        unordered_map<char, int> c_pos;
+        int maxLen = 0;
         int start = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (ch_pos.find(s[i]) == ch_pos.end()) {
-                ch_pos[s[i]] = i;
-            }
+            if (c_pos.find(s[i]) == c_pos.end() || c_pos[s[i]] < start)
+                c_pos[s[i]] = i;
             else {
-                res = max(res, i - start);
-                for (int j = start; j < ch_pos[s[i]]; j++) {
-                    ch_pos.erase(s[j]);
-                }
-                start = ch_pos[s[i]] + 1;
-                ch_pos[s[i]] = i;
+                maxLen = max(maxLen, i - start);
+                start = c_pos[s[i]] + 1;
+                c_pos[s[i]] = i;
             }
         }
-        res = max(res, (int)(s.length() - start));
-        return res;
+        maxLen = max(maxLen, (int)s.length() - start);
+        return maxLen;
     }
 };
