f312678a1c7c6bdec2161dc227ebf14137ced91a
Shijie Ma <ma.1371sao@gmail.com>
2018-04-08 15:39:33 -0400

    Add files via upload

--- LC019.cpp
+++ LC019.cpp
@@ -1,25 +1,27 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* removeNthFromEnd(ListNode* head, int n) {
-		ListNode* preHead = new ListNode(0);
-		preHead->next = head;
-		ListNode* fast = preHead;
-		ListNode* slow = fast;
-		for (int i = 0; i < n + 1; i++)
-			fast = fast->next;
-		while (fast) {
-			fast = fast->next;
-			slow = slow->next;
-		}
-		slow->next = slow->next->next;
-		return preHead->next;
-	}
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* preHead = new ListNode(0);
+        preHead->next = head;
+        ListNode* p = preHead;
+        for (int i = 0; i < n; i++) {
+            if (p->next == NULL) return head;
+            p = p->next;
+        }
+        ListNode* preNthNode = preHead;
+        while (p->next != NULL) {
+            p = p->next;
+            preNthNode = preNthNode->next;
+        }
+        preNthNode->next = preNthNode->next->next;
+        return preHead->next;
+    }
 };
