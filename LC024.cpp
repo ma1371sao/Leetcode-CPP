8edd3691c83f787fd5f62d13d366b8552dd2ff51
Shijie Ma <ma.1371sao@gmail.com>
2018-04-08 21:37:23 -0400

    Add files via upload

--- LC024.cpp
+++ LC024.cpp
@@ -1,26 +1,26 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* swapPairs(ListNode* head) {
-		ListNode* preHead = new ListNode(0);
-		preHead->next = head;
-		ListNode* p = head;
-		ListNode* pre = preHead;
-		while (p && p->next) {
-			ListNode* nxtPair = p->next->next;
-			p->next->next = p;
-			pre->next = p->next;
-			pre = p;
-			p = nxtPair;
-		}
-		pre->next = p;
-		return preHead->next;
-	}
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* preHead = new ListNode(0);
+        preHead->next = head;
+        ListNode* p = head;
+        ListNode* pre = preHead;
+        while (p && p->next) {
+            pre->next = p->next;
+            ListNode* nxt = p->next->next;
+            p->next->next = p;
+            p->next = nxt;
+            pre = p;
+            p = nxt;
+        }
+        return preHead->next;
+    }
 };
