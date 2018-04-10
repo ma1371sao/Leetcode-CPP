37a3ff6db43adb053eda7cbe8e29b35275622afa
Shijie Ma <ma.1371sao@gmail.com>
2018-04-10 02:10:02 -0400

    Add files via upload

--- LC029.cpp
+++ LC029.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-	int divide(int dividend, int divisor) {
-		int ans = 0;
-		int flag = 0;
-		if (dividend == 0x80000000 && divisor == -1)
-			return 0x7fffffff;
-		if (dividend == 0)
-			return 0;
-		if ((dividend<0 && divisor<0) || (dividend>0 && divisor>0))
-			flag = 1;
-		else
-			flag = -1;
-		long long did = llabs((long long)dividend);
-		long long dir = llabs((long long)divisor);
-		long long a[100000];
-		a[0] = dir;
-		int max;
-		for (max = 0; a[max] <= did; max++)
-		{
-			dir = dir << 1;
-			a[max + 1] = dir;
-		}
-		long long cur = 0;
-		for (int i = max - 1; i >= 0; i--)
-		{
-			while (cur + a[i] <= did)
-			{
-				cur = cur + a[i];
-				ans = ans + pow(2, i);
-			}
-		}
-		if (flag == 1)
-			return ans;
-		if (flag == -1)
-			return -ans;
-		return ans;
-	}
+    int divide(int dividend, int divisor) {
+        if (dividend == 0) return 0;
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if (dividend == INT_MIN && divisor == 1) return INT_MIN;
+        vector<pair<long long, int>> factor;
+        int sign;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+            sign = -1;
+        else
+            sign = 1;
+        long long did = llabs((long long)dividend);
+        long long dis = llabs((long long)divisor);
+        factor.push_back(make_pair(dis, 1));
+        int n = 1;
+        while (1) {
+            dis = (dis << 1);
+            if (dis > did) break;
+            n += n;
+            factor.push_back(make_pair(dis, n));
+        }
+        int res = 0;
+        for (int i = factor.size() - 1; i >= 0; i--) {
+            if (did - factor[i].first >= 0) {
+                res += factor[i].second;
+                did -= factor[i].first;
+            }
+        }
+        return res * sign;
+    }
 };
