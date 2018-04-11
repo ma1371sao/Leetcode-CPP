9286b63562a8895612fb4e87cfd7c465d591a92b
Shijie Ma <ma.1371sao@gmail.com>
2018-04-11 17:22:02 -0400

    Add files via upload

--- LC060.cpp
+++ LC060.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
-	string getPermutation(int n, int k) {
-		if (k == 0)
-			return "";
-		int vis[10000];
-		memset(vis, 0, sizeof(vis));
-		string ans;
-		for (int pos = 0; pos<n - 1; pos++)
-		{
-			int minnum = ((k - 1) / factorial(n - 1 - pos)) % (n - pos) + 1;
-			int num, i;
-			char c;
-			for (i = 0; i<n && minnum>0; i++)
-			{
-				if (vis[i] == 0)
-				{
-					minnum--;
-					if (minnum == 0)
-					{
-						num = i + 1;
-						c = '0' + num;
-						vis[i] = 1;
-					}
-				}
-			}
-			ans.push_back(c);
-		}
-		for (int i = 0; i<n; i++)
-			if (vis[i] == 0)
-			{
-				char c = '0' + i + 1;
-				ans.push_back(c);
-				break;
-			}
-		return ans;
-	}
-	int factorial(int k)
-	{
-		if (k == 0)
-			return 1;
-		int ans = 1;
-		for (int i = 1; i <= k; i++)
-			ans = ans*i;
-		return ans;
-	}
+    string getPermutation(int n, int k) {
+        vector<int> f(n + 1, 1);
+        string res;
+        list<int> nums;
+        for (int i = 1; i <= n; i++)
+            nums.push_back(i);
+        for (int i = 2; i <= n; i++)
+            f[i] = i * f[i - 1];
+        for (int i = 1; i < n; i++) {
+            int num = (k - 1) / f[n - i];
+            auto it = nums.begin();
+            for (int j = 0; j < num; j++, it++);
+            res.push_back(*it + '0');
+            k -= num * f[n - i];
+            nums.erase(it);
+        }
+        res.push_back(*nums.begin() + '0');
+        return res;
+    }
 };
