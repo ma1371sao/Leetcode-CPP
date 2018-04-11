c89dc7288e6ba49b8d6105ed817aa7483e7ffdd6
Shijie Ma <ma.1371sao@gmail.com>
2018-04-11 18:07:27 -0400

    Add files via upload

--- LC061.cpp
+++ LC061.cpp
@@ -1,32 +1,34 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* rotateRight(ListNode* head, int k) {
-		if (k == 0 || head == NULL)
-			return head;
-		int n = 1;
-		ListNode* p = head;
-		while (p->next)
-		{
-			n++;
-			p = p->next;
-		}
-		ListNode* oriEnd = p;
-		k = k%n;
-		if (k == 0)
-			return head;
-		int save = n - k;
-		for (p = head; save>1; p = p->next, save--);
-		oriEnd->next = head;
-		head = p->next;
-		p->next = NULL;
-		return head;
-	}
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == NULL) return head;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        int n = 0;
+        while (fast) {
+            n++;
+            fast = fast->next;
+        }
+        k = k % n;
+        if (k == 0) return head;
+        fast = head;
+        for (int i = 0; i < k; i++)
+            fast = fast->next;
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        fast->next = head;
+        head = slow->next;
+        slow->next = NULL;
+        return head;
+    }
 };
