66284234fce1f4a81b65b2199787ca8f79479d2c
Shijie Ma <ma.1371sao@gmail.com>
2018-04-11 14:56:57 -0400

    Add files via upload

--- LC055.cpp
+++ LC055.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-	bool canJump(vector<int>& nums) {
-		if (nums.size() == 0 || nums.size() == 1)
-			return true;
-		return DFS(nums, nums[0], 0);
-	}
-	bool DFS(vector<int>& nums, int pos, int start)
-	{
-		if (pos >= nums.size() - 1)
-			return true;
-		int max = pos + nums[pos];
-		int s = pos;
-		for (int j = pos - 1; j>start; j--)
-		{
-			if (max<nums[j] + j)
-			{
-				max = nums[j] + j;
-				s = j;
-			}
-		}
-		if (max>pos)
-			if (DFS(nums, max, s))
-				return true;
-		return false;
-	}
+    bool canJump(vector<int>& nums) {
+        int right = 0;
+        for (int i = 0; i < nums.size() && i <= right; i++)
+            right = max(right, nums[i] + i);
+        if (right >= nums.size() - 1) return true;
+        return false;
+    }
 };
