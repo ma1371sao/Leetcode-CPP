00c3991dd85d49d55be257f2f51ad48ebee1c5fb
Shijie Ma <ma.1371sao@gmail.com>
2018-04-12 12:07:50 -0400

    Add files via upload

--- LC077.cpp
+++ LC077.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-	vector<vector<int>> combine(int n, int k) {
-		vector<vector<int>> ans;
-		if (k>n)
-			return ans;
-		int a[100000];
-		DFS(0, 1, n, k, a, ans);
-		return ans;
-	}
-	void DFS(int num, int pos, int n, int k, int a[], vector<vector<int>>& ans)
-	{
-		if (num == k)
-		{
-			vector<int> v;
-			for (int i = 0; i<k; i++)
-				v.push_back(a[i]);
-			ans.push_back(v);
-			return;
-		}
-		if (pos>n)
-			return;
-		a[num] = pos;
-		DFS(num + 1, pos + 1, n, k, a, ans);
-		DFS(num, pos + 1, n, k, a, ans);
-	}
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> v;
+        findCombine(res, v, n, k, 1);
+        return res;
+    }
+    
+    void findCombine(vector<vector<int>>& res, vector<int>& v, int n, int k, int pos) {
+        if (k == 0) {
+            res.push_back(v);
+            return;
+        }
+        if (pos > n) return;
+        v.push_back(pos);
+        findCombine(res, v, n, k - 1, pos + 1);
+        v.pop_back();
+        findCombine(res, v, n, k, pos + 1);
+    }
 };
