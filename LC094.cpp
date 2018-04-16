efc0ffacf580d7ac49f83091069233161bd381b7
Shijie Ma <ma.1371sao@gmail.com>
2018-04-16 10:55:17 -0400

    Add files via upload

--- LC094.cpp
+++ LC094.cpp
@@ -1,3 +1,5 @@
+// 1. Recursive solution
+
 /**
 * Definition for a binary tree node.
 * struct TreeNode {
@@ -20,4 +22,41 @@ public:
 		ans.push_back(p->val);
 		if (p->right) DFS(p->right);
 	}
+};
+
+// 2. Iterative solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == NULL) return res;
+        stack<TreeNode*> nodes;
+        while (root) {
+            nodes.push(root);
+            root = root->left;
+        }
+        while (!nodes.empty()) {
+            root = nodes.top();
+            res.push_back(root->val);
+            nodes.pop();
+            if (root->right) {
+                root = root->right;
+                while (root) {
+                    nodes.push(root);
+                    root = root->left;
+                }
+            }
+        }
+        return res;
+    }
 };
