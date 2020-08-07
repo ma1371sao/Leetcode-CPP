d4a0a69ccfae08b774fe5bf69d844bf000920494
Shijie Ma <ma.1371sao@gmail.com>
2020-08-07 15:23:33 -0700

    add LC110.cpp

--- LC110.cpp
+++ LC110.cpp
@@ -1,44 +1,28 @@
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	bool isBalanced(TreeNode* root) {
-		if (root == NULL)
-			return true;
-		if (DFS(root) == -1)
-			return false;
-		else
-			return true;
-	}
-	int DFS(TreeNode* t)
-	{
-		int ln = 0, rn = 0;
-		if (t->left)
-		{
-			ln = DFS(t->left) + 1;
-			if (ln == 0)
-				return -1;
-		}
-		if (t->right)
-		{
-			rn = DFS(t->right) + 1;
-			if (rn == 0)
-				return -1;
-		}
-		if ((ln - rn)<-1 || (ln - rn)>1)
-			return -1;
-		int d;
-		if (ln >= rn)
-			d = ln;
-		else
-			d = rn;
-		return d;
-	}
+    bool isBalanced(TreeNode* root) {
+        int d = dfs(root);
+        if (d == -1) return false;
+        return true;
+    }
+    
+    int dfs(TreeNode* root) {
+        if (root == NULL) return 0;
+        int leftD = dfs(root->left);
+        int rightD = dfs(root->right);
+        if (leftD == -1 || rightD == -1) return -1;
+        if (abs(rightD - leftD) > 1) return -1;
+        return 1 + max(leftD, rightD);
+    }
 };
