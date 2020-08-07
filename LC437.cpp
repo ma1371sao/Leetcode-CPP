347c672e7dfbc2c4b6a37fb8b64eaaf6596820e9
Shijie Ma <ma.1371sao@gmail.com>
2020-08-06 18:40:22 -0700

    add LC437.cpp

--- LC437.cpp
+++ LC437.cpp
@@ -1,25 +1,29 @@
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	int pathSum(TreeNode* root, int sum) {
-		if (root == NULL) return 0;
-		return dfs(root, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
-	}
-	int dfs(TreeNode* root, int sum) {
-		int res = 0;
-		if (root->val == sum) res++;
-		int left = 0;
-		int right = 0;
-		if (root->left) left = dfs(root->left, sum - root->val);
-		if (root->right) right = dfs(root->right, sum - root->val);
-		return res + left + right;
-	}
+    int pathSum(TreeNode* root, int sum) {
+        if (root == NULL) return 0;
+        return dfs(root, sum, 0, 0) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    }
+    
+    int dfs(TreeNode* root, int sum, int val, int num) {
+        val += root->val;
+        if (val == sum) num++;
+        
+        if (root->left)
+            num = dfs(root->left, sum, val, num);
+        if (root->right)
+            num = dfs(root->right, sum, val, num);
+        return num;
+    }
 };
