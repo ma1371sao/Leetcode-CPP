28ffc76711579b8cb50a6bccf5de8776341fbc03
Shijie Ma <ma.1371sao@gmail.com>
2020-08-12 19:03:52 -0700

    add LC075.cpp

--- LC075.cpp
+++ LC075.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-	void sortColors(vector<int>& nums) {
-		int n = nums.size();
-		if (n == 0)
-			return;
-		qsort(0, n - 1, nums);
-	}
-	void qsort(int left, int right, vector<int>& nums)
-	{
-		if (left<right)
-		{
-			int i = left;
-			int j = right;
-			do
-			{
-				for (i = i + 1; i<right && nums[i]<nums[left]; i++);
-				for (; j>left && nums[j]>nums[left]; j--);
-				if (i<j)
-				{
-					int tmp = nums[i];
-					nums[i] = nums[j];
-					nums[j] = tmp;
-				}
-			} while (i<j);
-			if (j>left)
-			{
-				int tmp = nums[left];
-				nums[left] = nums[j];
-				nums[j] = tmp;
-			}
-			qsort(left, j - 1, nums);
-			qsort(j + 1, right, nums);
-		}
-	}
+    void sortColors(vector<int>& nums) {
+        int zero = 0;
+        int two = nums.size() - 1;
+        for (int i = 0; i <= two && i >= zero; i++) {
+            while (i <= two && i >= zero) {
+                if (nums[i] == 0) {
+                    nums[i] = nums[zero];
+                    nums[zero] = 0;
+                    zero++;
+                } else if (nums[i] == 2) {
+                    nums[i] = nums[two];
+                    nums[two] = 2;
+                    two--;
+                } else
+                    break;
+            }
+        }
+    }
 };
