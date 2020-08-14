cfd734680f209ce205ad04fb5bec08df1fe00d64
Shijie Ma <ma.1371sao@gmail.com>
2020-08-13 22:46:05 -0700

    add LC139.cpp

--- LC139.cpp
+++ LC139.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dictSet(wordDict.begin(), wordDict.end());
-        vector<vector<bool>> canSegment(s.length(), vector<bool>(s.length(), false));
-        for (int i = 0; i < s.length(); i++) {
-            if (dictSet.find(s.substr(0, i + 1)) != dictSet.end())
-                canSegment[0][i] = true;
+        unordered_set<string> dictSet;
+        for (string word : wordDict)
+            dictSet.insert(word);
+        
+        int n = s.length();
+        vector<bool> dp(n, false);
+        for (int i = 0; i < n; i++) {
+            if (dictSet.count(s.substr(0, i + 1)))
+                dp[i] = true;
         }
-        for (int i = 1; i < s.length(); i++) {
-            if (!canSegment[0][i]) {
-                for (int j = i - 1; j >= 0; j--) {
-                    if (canSegment[0][j] && dictSet.find(s.substr(j + 1, i - j)) != dictSet.end()) {
-                        canSegment[0][i] = true;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j]) {
+                    if (dictSet.count(s.substr(j + 1, i - j))) {
+                        dp[i] = true;
                         break;
                     }
                 }
             }
         }
-        return canSegment[0][s.length() - 1];
+        return dp[n - 1];
     }
 };
