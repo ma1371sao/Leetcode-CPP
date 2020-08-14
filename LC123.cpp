d086500ff3b3905680426d5a19883649719dea9b
Shijie Ma <ma.1371sao@gmail.com>
2020-08-13 22:21:10 -0700

    add LC123.cpp

--- LC123.cpp
+++ LC123.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//DP
+class Solution1 {
 public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() <= 1) return 0;
@@ -11,5 +12,40 @@ public:
             }
         }
         return dp[2][prices.size() - 1];
+    }
+};
+
+//Divide and conquer
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n < 2) return 0;
+        int maxPft = 0;
+        int minP = prices[0];
+        vector<int> maxPftLeft(n, 0);
+        vector<int> maxPftRight(n, 0);
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > minP)
+                maxPft = max(maxPft, prices[i] - minP);
+            else
+                minP = prices[i];
+            maxPftLeft[i] = maxPft;
+        }
+        
+        int maxP = prices[n - 1];
+        maxPft = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            if (prices[i] < maxP)
+                maxPft = max(maxPft, maxP - prices[i]);
+            else
+                maxP = prices[i];
+            maxPftRight[i] = maxPft;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            maxPft = max(maxPft, maxPftLeft[i] + maxPftRight[i]);
+        }
+        return maxPft;
     }
 };
