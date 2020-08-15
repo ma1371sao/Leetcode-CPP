42feeb244db6634941b9209a6034d490db2056f2
Shijie Ma <ma.1371sao@gmail.com>
2020-08-15 12:58:54 -0700

    update LC852.cpp

--- LC852.cpp
+++ LC852.cpp
@@ -18,3 +18,27 @@ public:
         return -1;
     }
 };
+
+//If there are dup such as [0,1,1,1,2,1,1,0,0,0,0]
+class Solution1 {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int l = 0, r = A.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (mid == 0) l++;
+            else if (mid == A.size() - 1) r--;
+            else {
+                if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1])
+                    return mid;
+                else if (A[mid] > A[mid - 1] || A[mid + 1] > A[mid])
+                    l = mid + 1;
+                else if (A[mid] < A[mid + 1] || A[mid - 1] < A[mid])
+                    r = mid - 1;
+                else
+                    r--;
+            }
+        }
+        return -1;
+    }
+};
