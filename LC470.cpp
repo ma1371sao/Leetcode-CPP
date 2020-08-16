d5a84de4228894c5480bb1668d1066bc93795b7f
Shijie Ma <ma.1371sao@gmail.com>
2020-08-15 21:33:32 -0700

    add LC470.cpp

---
+++ LC470.cpp
@@ -0,0 +1,13 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        while (true) {
+            int num = (rand7() - 1)* 7 + rand7();
+            if (num <= 40) return num % 10 + 1;
+        }
+    }
+};
