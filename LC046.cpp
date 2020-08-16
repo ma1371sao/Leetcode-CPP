570e0ea26d90d3c555d213d9003dd34d1fc0743f
Shijie Ma <ma.1371sao@gmail.com>
2020-08-15 20:53:04 -0700

    add LC046.cpp

--- LC046.cpp
+++ LC046.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-	vector<vector<int>> ans;
-	int vis[100];
-	int a[100];
-	vector<vector<int>> permute(vector<int>& nums) {
-		if (nums.size() == 0)
-			return ans;
-		//sort(nums.begin(),nums.end());
-		for (int i = 0; i<nums.size(); i++)  vis[i] = 0;
-		DFS(nums, 0);
-		return ans;
-	}
-	void DFS(vector<int>& nums, int pos)
-	{
-		if (pos == nums.size())
-		{
-			vector<int> v;
-			for (int i = 0; i<pos; i++)
-				v.push_back(a[i]);
-			ans.push_back(v);
-			return;
-		}
-		for (int i = 0; i<nums.size(); i++)
-		{
-			if (vis[i] == 1)
-				continue;
-			else
-			{
-				a[pos] = nums[i];
-				vis[i] = 1;
-				DFS(nums, pos + 1);
-				vis[i] = 0;
-			}
-		}
-	}
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<bool> visited(nums.size(), false);
+        vector<int> perm;
+        dfs(nums, res, visited, perm);
+        return res;
+    }
+    
+    void dfs(vector<int>& nums, vector<vector<int>>& res, vector<bool>& visited, vector<int>& perm) {
+        if (perm.size() == nums.size()) {
+            res.push_back(perm);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (!visited[i]) {
+                perm.push_back(nums[i]);
+                visited[i] = true;
+                dfs(nums, res, visited, perm);
+                perm.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
 };
