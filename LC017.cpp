2fbbc0eae003b5f2f0a04841fd8871c9620ab9ec
Shijie Ma <ma.1371sao@gmail.com>
2020-08-17 00:01:40 -0700

    add LC017.cpp

--- LC017.cpp
+++ LC017.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
-	vector<string> ans;
-	int n;
-	char sans[1000000];
-	map<char, string> m;
-	vector<string> letterCombinations(string digits) {
-		m['2'] = "abc";
-		m['3'] = "def";
-		m['4'] = "ghi";
-		m['5'] = "jkl";
-		m['6'] = "mno";
-		m['7'] = "pqrs";
-		m['8'] = "tuv";
-		m['9'] = "wxyz";
-		n = digits.length();
-		if (n == 0)
-			return ans;
-		DFS(digits, 0);
-		return ans;
-	}
-	void DFS(string digits, int poi)
-	{
-		if (poi == n)
-		{
-			string s;
-			for (int i = 0; i<poi; i++)
-				if (sans[i] != ' ')
-					s.push_back(sans[i]);
-			ans.push_back(s);
-		}
-		if (digits[poi] == '1')
-		{
-			sans[poi] = ' ';
-			DFS(digits, poi + 1);
-		}
-		else
-		{
-			string str = m[digits[poi]];
-			for (int i = 0; i<str.length(); i++)
-			{
-				sans[poi] = str[i];
-				DFS(digits, poi + 1);
-			}
-		}
-	}
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if (digits == "") return ans;
+        unordered_map<char, string> digLet;
+        digLet['2'] = "abc";
+        digLet['3'] = "def";
+        digLet['4'] = "ghi";
+        digLet['5'] = "jkl";
+        digLet['6'] = "mno";
+        digLet['7'] = "pqrs";
+        digLet['8'] = "tuv";
+        digLet['9'] = "wxyz";
+        string s = "";
+        dfs(digLet, digits, 0, s, ans);
+        return ans;
+    }
+    
+    void dfs(unordered_map<char, string>& digLet, string& digits, int idx, string s, vector<string>& ans) {
+        if (idx == digits.length()) {
+            ans.push_back(s);
+            return;
+        }
+        
+        if (digits[idx] == '1') dfs(digLet, digits, idx + 1, s, ans);
+        else {
+            for (char c : digLet[digits[idx]]) {
+                dfs(digLet, digits, idx + 1, s + c, ans);
+            }
+        }
+    }
 };
