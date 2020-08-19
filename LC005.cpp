25bd1f4df9a05b099dcafa7bd1398207d2b56322
Shijie Ma <ma.1371sao@gmail.com>
2020-08-19 14:16:15 -0700

    add LC005.cpp

--- LC005.cpp
+++ LC005.cpp
@@ -1,57 +1,62 @@
-class Solution {
+class Solution1 {
 public:
     string longestPalindrome(string s) {
-        if (s.length() == 0) return "";
-        int left = 0;
-        int right = s.length() - 1;
-        int maxLen = 0;
-        int start = 0;
-        for (int l = left; l <= right; l++) {
-            int mid = l + (right - l)/2;
-            if (right - l + 1 <= maxLen)
-                break;
-            if ((right - l + 1) % 2) {
-                int half = 0;
-                for (; mid + half < s.length() && mid - half >= l && s[mid + half] == s[mid - half]; half++);
-                half--;
-                if (half * 2 + 1 > maxLen) {
-                    maxLen = half * 2 + 1;
-                    start = mid - half;
+        int n = s.length();
+        if (n <= 1) return s;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int maxLen = 1;
+        string ans = s.substr(0, 1);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) dp[i][j] = true;
+                else if (s[i] == s[j]) {
+                    if (j == i + 1) dp[i][j] = true;
+                    else if (dp[i + 1][j - 1]) dp[i][j] = true;
                 }
-            }
-            else {
-                int half = 0;
-                for (; mid + 1 + half < s.length() && mid - half >= l && s[mid + 1 + half] == s[mid - half]; half++);
-                half--;
-                if (half * 2 + 2 > maxLen) {
-                    maxLen = half * 2 + 2;
-                    start = mid - half;
+                
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    ans = s.substr(i, maxLen);
                 }
             }
         }
-        for (int r = right; r >= left; r--) {
-            int mid = left + (r - left) / 2;
-            if (r - left + 1 <= maxLen)
-                break;
-            if ((r - left + 1) % 2) {
-                int half = 0;
-                for (; mid - half >= left && mid + half <= r && s[mid + half] == s[mid - half]; half++);
-                half--;
-                if (half * 2 + 1 > maxLen) {
-                    maxLen = half * 2 + 1;
-                    start = mid - half;
-                }
+        return ans;
+    }
+};
+
+class Solution2 {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n <= 1) return s;
+        string ans;
+        ans.push_back(s[0]);
+        int maxLen = 1;
+        for (int i = 0; i < n; i++) {
+            int l = i - 1;
+            int r = i + 1;
+            for (; l >= 0 && r < n && s[l] == s[r]; l--, r++);
+            l++;
+            r--;
+            int len = r - l + 1;
+            if (maxLen < len) {
+                maxLen = len;
+                ans = s.substr(l, maxLen);
             }
-            else {
-                int half = 0;
-                for (; mid + 1 + half <= r && mid - half >= left && s[mid + 1 + half] == s[mid - half]; half++);
-                half--;
-                if (half * 2 + 2 > maxLen) {
-                    maxLen = half * 2 + 2;
-                    start = mid - half;
+            
+            if (i + 1 < n && s[i] == s[i + 1]) {
+                int l = i - 1;
+                int r = i + 2;
+                for (; l >= 0 && r < n && s[l] == s[r]; l--, r++);
+                l++;
+                r--;
+                int len = r - l + 1;
+                if (maxLen < len) {
+                    maxLen = len;
+                    ans = s.substr(l, maxLen);
                 }
             }
         }
-        return s.substr(start, maxLen);
+        return ans;
     }
 };
