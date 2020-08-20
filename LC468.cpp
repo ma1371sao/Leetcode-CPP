8bf7cb866a2b0a8a784d4982829c613782f2f23f
Shijie Ma <ma.1371sao@gmail.com>
2020-08-19 18:26:51 -0700

    add LC468.cpp

--- LC468.cpp
+++ LC468.cpp
@@ -1,61 +1,46 @@
 class Solution {
 public:
-	string validIPAddress(string IP) {
-		int numSign = 0;
-		int start = 0;
-		if (IP.find('.') != string::npos) {
-			if (IP.find(':') != string::npos) return "Neither";
-			while (IP.find('.', start) != string::npos) {
-				numSign++;
-				if (numSign > 3) return "Neither";
-				int posSign = IP.find('.', start);
-				if (start == posSign) return "Neither";
-				string num = IP.substr(start, posSign - start);
-				if (num.length() > 1 && num[0] == '0') return "Neither";
-				if (num.length() > 3) return "Neither";
-				for (int i = 0; i < num.length(); i++) {
-					if (num[i] < '0' || num[i] > '9')
-						return "Neither";
-				}
-				int numInt = stoi(num);
-				if (numInt < 0 || numInt > 255) return "Neither";
-				start = posSign + 1;
-			}
-			if (numSign != 3 || start >= IP.length()) return "Neither";
-			string num = IP.substr(start);
-			if (num.length() > 1 && num[0] == '0') return "Neither";
-			if (num.length() > 3) return "Neither";
-			for (int i = 0; i < num.length(); i++) {
-				if (num[i] < '0' || num[i] > '9')
-					return "Neither";
-			}
-			int numInt = stoi(num);
-			if (numInt < 0 || numInt > 255) return "Neither";
-			return "IPv4";
-		}
-		if (IP.find(':') != string::npos) {
-			while (IP.find(':', start) != string::npos) {
-				numSign++;
-				if (numSign > 7) return "Neither";
-				int posSign = IP.find(':', start);
-				if (start == posSign) return "Neither";
-				string num = IP.substr(start, posSign - start);
-				if (num.length() > 4) return "Neither";
-				for (int i = 0; i < num.length(); i++) {
-					if (!((num[i] >= '0' && num[i] <= '9') || (num[i] >= 'a' && num[i] <= 'f') || (num[i] >= 'A' && num[i] <= 'F')))
-						return "Neither";
-				}
-				start = posSign + 1;
-			}
-			if (numSign != 7 || start >= IP.length()) return "Neither";
-			string num = IP.substr(start);
-			if (num.length() > 4) return "Neither";
-			for (int i = 0; i < num.length(); i++) {
-				if (!((num[i] >= '0' && num[i] <= '9') || (num[i] >= 'a' && num[i] <= 'f') || (num[i] >= 'A' && num[i] <= 'F')))
-					return "Neither";
-			}
-			return "IPv6";
-		}
-		return "Neither";
-	}
+    string validIPAddress(string IP) {
+        if (IP.find(".") != -1) {
+            int start = 0;
+            for (int i = 0; i < 4; i++) {
+                int idx = IP.find(".", start);
+                if (i < 3 && idx == -1) return "Neither";
+                if (i == 3) {
+                    if (idx != -1) return "Neither";
+                    idx = IP.length();
+                }
+                
+                string sub = IP.substr(start, idx - start);
+                if ((sub[0] == '0' && sub.length() > 1) || sub.length() > 3 || sub.length() == 0) return "Neither";
+                for (int i = 0; i < sub.length(); i++) {
+                    if (sub[i] < '0' || sub[i] > '9') return "Neither";
+                }
+                int d = stoi(sub);
+                if (d > 255) return "Neither";
+                start = idx + 1;
+            }
+            return "IPv4";
+        }
+        int start = 0;
+        for (int i = 0; i < 8; i++) {
+            int idx = IP.find(":", start);
+            if (i < 7 && idx == -1) return "Neither";
+            if (i == 7) {
+                if (idx != -1) return "Neither";
+                idx = IP.length();
+            }
+            
+            string sub = IP.substr(start, idx - start);
+            if (sub.length() > 4 || sub.length() == 0) return "Neither";
+            for (int i = 0; i < sub.length(); i++) {
+                if (!(
+                    (sub[i] >= '0' && sub[i] <= '9') || (sub[i] >= 'a' && sub[i] <= 'f') || (sub[i] >= 'A' && sub[i] <= 'F')
+                ))
+                    return "Neither";
+            }
+            start = idx + 1;
+        }
+        return "IPv6";
+    }
 };
