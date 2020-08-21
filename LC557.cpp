c81e6d236cd88ac77089ce12b355c36fed392407
Shijie Ma <ma.1371sao@gmail.com>
2020-08-20 21:48:53 -0700

    add LC557.cpp

--- LC557.cpp
+++ LC557.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-	string reverseWords(string s) {
-		string res;
-		int space = s.find(' ');
-		int start = 0;
-		while (space != string::npos) {
-			if (start > 0) res += " ";
-			string sub = s.substr(start, space - start);
-			reverse(sub.begin(), sub.end());
-			res += sub;
-			start = space + 1;
-			space = s.find(' ', start);
-		}
-
-		string sub = s.substr(start);
-		reverse(sub.begin(), sub.end());
-		if (start > 0) res += " ";
-		res += sub;
-		return res;
-	}
+    string reverseWords(string s) {
+        int start = 0;
+        while (start < s.length()) {
+            int pos = s.find(" ", start);
+            if (pos == -1) pos = s.length();
+            for (int i = start, j = pos - 1; i < j; i++, j--) {
+                int tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+            }
+            start = pos + 1;
+        }
+        return s;
+    }
 };
