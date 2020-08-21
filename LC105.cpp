f216e86a338e8c662513852802b9ae3dbe9efa13
Shijie Ma <ma.1371sao@gmail.com>
2020-08-20 19:49:31 -0700

    add LC105.cpp

--- LC105.cpp
+++ LC105.cpp
@@ -4,44 +4,31 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    int idx = 0;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size();
         if (n == 0) return NULL;
-        TreeNode* root = new TreeNode(preorder[0]);
-        unordered_map<int, int> val_inPos;
+        unordered_map<int, int> val_idx;
         for (int i = 0; i < n; i++)
-            val_inPos[inorder[i]] = i;
-        int pos = 0;
-        if (val_inPos[preorder[0]] > 0)
-            pos = construct(root, val_inPos, preorder, inorder, pos + 1, 0, val_inPos[preorder[pos]]);
-        if (val_inPos[preorder[0]] < preorder.size() - 1)
-            construct(root, val_inPos, preorder, inorder, pos + 1, 1, n);
-        return root;
+            val_idx[inorder[i]] = i;
+        return dfs(preorder, val_idx, 0, n - 1);
     }
     
-    int construct(TreeNode* root, unordered_map<int, int>& val_inPos, vector<int>& preorder, vector<int>& inorder, int pos,
-                  int flag, int rightLimit) {
-        if (flag == 0) {
-            root->left = new TreeNode(preorder[pos]);
-            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < val_inPos[preorder[pos]])
-                pos = construct(root->left, val_inPos, preorder, inorder, pos + 1, 0, val_inPos[preorder[pos]]);
-            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < rightLimit)
-                pos = construct(root->left, val_inPos, preorder, inorder, pos + 1, 1, rightLimit);
-            return pos;
-        }
-        else {
-            cout << pos << endl;
-            root->right = new TreeNode(preorder[pos]);
-            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < val_inPos[preorder[pos]])
-                pos = construct(root->right, val_inPos, preorder, inorder, pos + 1, 0, val_inPos[preorder[pos]]);
-            if (pos + 1 < preorder.size() && val_inPos[preorder[pos + 1]] < rightLimit)
-                pos = construct(root->right, val_inPos, preorder, inorder, pos + 1, 1, rightLimit);
-            return pos;
-        }
+    TreeNode* dfs(vector<int>& preorder, unordered_map<int, int>& val_idx, int l, int r) {
+        if (l > r) return NULL;
+        int val = preorder[idx];
+        int pos = val_idx[val];
+        if (pos < l || pos > r) return NULL;
+        idx++;
+        TreeNode* left = dfs(preorder, val_idx, l, pos - 1);
+        TreeNode* right = dfs(preorder, val_idx, pos + 1, r);
+        return new TreeNode(val, left, right);
     }
 };
