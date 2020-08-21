8b53a62f33c415a458c4b9d862ec8930871ee79b
Shijie Ma <ma.1371sao@gmail.com>
2020-08-20 20:30:16 -0700

    add LC151.cpp

--- LC151.cpp
+++ LC151.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-	void reverseWords(string &s) {
-		int pos = 0;
-		bool empty = false;
-		for (int i = 0; i < s.length(); i++) {
-			if (s[i] != ' ') {
-				empty = true;
-				s[pos++] = s[i];
-			}
-			else if (empty) {
-				s[pos++] = s[i];
-				empty = false;
-			}
-		}
-		if (pos == 0) {
-			s = "";
-			return;
-		}
-		if (s[pos - 1] == ' ') pos--;
-		s = s.substr(0, pos);
-		reverse(s.begin(), s.end());
-		int start = 0;
-		while (1) {
-			int pos = s.find(' ', start);
-			if (pos == string::npos) {
-				reverse(s.begin() + start, s.end());
-				break;
-			}
-			else {
-				reverse(s.begin() + start, s.begin() + pos);
-				start = pos + 1;
-			}
-		}
-	}
+    string reverseWords(string s) {
+        bool allowEmpty = false;
+        int n = s.length();
+        int pos = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ' ') {
+                allowEmpty = true;
+                s[pos++] = s[i];
+            } else {
+                if (allowEmpty) {
+                    s[pos++] = s[i];
+                    allowEmpty = false;
+                }
+            }
+        }
+        
+        if (pos == 0) return "";
+        s = s.substr(0, pos);
+        if (s[pos - 1] == ' ') s.pop_back();
+        n = s.length();
+        reverse(s, 0, n - 1);
+        int start = 0;
+        while (start < n) {
+            int pos = s.find(" ", start);
+            if (pos == -1) pos = n;
+            reverse(s, start, pos - 1);
+            start = pos + 1;
+        }
+        return s;
+    }
+    
+    void reverse(string& s, int i, int j) {
+        for (; i < j; i++, j--) {
+            int tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
 };
