4355f53482552bed2e8e990bd696ec178cd89749
Shijie Ma <ma.1371sao@gmail.com>
2020-08-22 00:38:44 -0700

    add LC556.cpp

--- LC556.cpp
+++ LC556.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-	int nextGreaterElement(int n) {
-		vector<int> nums;
-		while (n) {
-			nums.push_back(n % 10);
-			n /= 10;
-		}
-		reverse(nums.begin(), nums.end());
-		int pos = nums.size() - 1;
-		for (; pos > 0; pos--) {
-			if (nums[pos - 1] < nums[pos])
-				break;
-		}
-		if (pos == 0) return -1;
-		int min = pos;
-		for (int j = pos; j < nums.size(); j++) {
-			if (nums[j] > nums[pos - 1] && nums[j] < nums[min])
-				min = j;
-		}
-		int temp = nums[pos - 1];
-		nums[pos - 1] = nums[min];
-		nums[min] = temp;
-		sort(nums.begin() + pos, nums.end());
-		long long res = 0;
-		for (int i = 0; i < nums.size(); i++) {
-			res += nums[i] * pow(10, nums.size() - 1 - i);
-		}
-		if (res > INT_MAX) return -1;
-		return res;
-	}
+    int nextGreaterElement(int n) {
+        if (n < 10) return -1;
+        vector<int> digits;
+        int cp_n = n;
+        while (cp_n > 0) {
+            digits.push_back(cp_n % 10);
+            cp_n /= 10;
+        }
+        
+        int len = digits.size();
+        for (int i = 0, j = len - 1; i < j; i++, j--) {
+            int tmp = digits[i];
+            digits[i] = digits[j];
+            digits[j] = tmp;
+        }
+        
+        int idx = len - 1;
+        for (; idx > 0 && digits[idx] <= digits[idx - 1]; idx--);
+        if (idx == 0) return -1;
+        int a = idx - 1;
+        for (idx = len - 1; idx > a && digits[idx] <= digits[a]; idx--);
+        int tmp = digits[a];
+        digits[a] = digits[idx];
+        digits[idx] = tmp;
+        
+        sort(digits.begin() + a + 1, digits.end());
+        long long ans = 0;
+        for (idx = len - 1; idx >= 0; idx--) {
+            ans += pow(10, len - 1 - idx) * digits[idx];
+            if (ans > INT_MAX) return -1;
+        }
+        return ans;
+    }
 };
