79fd8147706c2eb384316e75d23de847015013a5
Shijie Ma <ma.1371sao@gmail.com>
2020-08-21 21:09:21 -0700

    add LC127.cpp

--- LC127.cpp
+++ LC127.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-	int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-		wordList.erase(wordList.find(beginWord));
-		if (wordList.find(endWord) == wordList.end())  wordList.insert(endWord);
-		queue<pair<string, int>> q;
-		q.push(make_pair(beginWord, 1));
-		while (!q.empty()) {
-			pair<string, int> p = q.front();
-			q.pop();
-			if (p.first == endWord)  return p.second;
-			for (int i = 0; i<p.first.length(); i++) {
-				string tmp = p.first;
-				for (int j = 0; j<26; j++) {
-					tmp[i] = 'a' + j;
-					if (wordList.find(tmp) != wordList.end()) {
-						q.push(make_pair(tmp, p.second + 1));
-						wordList.erase(tmp);
-					}
-				}
-			}
-		}
-		return 0;
-	}
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordSet;
+        for (string word : wordList)
+            wordSet.insert(word);
+        if (!wordSet.count(endWord)) return 0;
+        queue<pair<string, int>> q;
+        q.push(make_pair(beginWord, 1));
+        unordered_set<string> visited;
+        visited.insert(beginWord);
+        
+        while (!q.empty()) {
+            string word = q.front().first;
+            int d = q.front().second;
+            q.pop();
+            
+            for (int i = 0; i < word.length(); i++) {
+                for (int j = 0; j < 26; j++) {
+                    if (word[i] == 'a' + j) continue;
+                    string s = word;
+                    s[i] = 'a' + j;
+                    if (wordSet.count(s) && !visited.count(s)) {
+                        q.push(make_pair(s, d + 1));
+                        visited.insert(s);
+                        
+                        if (s == endWord) {
+                            return d + 1;
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 };
