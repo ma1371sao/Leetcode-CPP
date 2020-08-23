8b25aa748c79a108add81797c753b00b067bba76
Shijie Ma <ma.1371sao@gmail.com>
2020-08-22 18:05:44 -0700

    add LC165.cpp

--- LC165.cpp
+++ LC165.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        if (version1 == "" && version2 == "") return 0;
-        if (version1 == "") version1 = "0";
-        if (version2 == "") version2 = "0";
-        int pointPos1 = version1.find('.');
-        string s1 = (pointPos1 == string::npos) ? version1 : version1.substr(0, pointPos1);
-        string rest1 = (pointPos1 == string::npos) ? "" : version1.substr(pointPos1 + 1);
-        int pointPos2 = version2.find('.');
-        string s2 = (pointPos2 == string::npos) ? version2 : version2.substr(0, pointPos2);
-        string rest2 = (pointPos2 == string::npos) ? "" : version2.substr(pointPos2 + 1);
-        if (stoi(s1) > stoi(s2)) return 1;
-        else if (stoi(s1) < stoi(s2)) return -1;
-        else return compareVersion(rest1, rest2);
+        int start1 = 0;
+        int start2 = 0;
+        int n1 = version1.length();
+        int n2 = version2.length();
+        int idx1 = 0, idx2 = 0;
+        while (idx1 < n1 || idx2 < n2) {
+            if (idx1 >= n1) {
+                idx2 = version2.find(".", start2);
+                if (idx2 == -1) idx2 = n2;
+                int v2 = stoi(version2.substr(start2, idx2 - start2));
+                
+                if (v2 != 0) return -1;
+                start2 = idx2 + 1;
+                
+            } else if (idx2 >= n2) {
+                idx1 = version1.find(".", start1);
+                if (idx1 == -1) idx1 = n1;
+                int v1 = stoi(version1.substr(start1, idx1 - start1));
+                
+                if (v1 != 0) return 1;
+                start1 = idx1 + 1;
+                
+            } else {
+                idx1 = version1.find(".", start1);
+                if (idx1 == -1) idx1 = n1;
+                int v1 = stoi(version1.substr(start1, idx1 - start1));
+
+                idx2 = version2.find(".", start2);
+                if (idx2 == -1) idx2 = n2;
+                int v2 = stoi(version2.substr(start2, idx2 - start2));
+
+                if (v1 > v2) return 1;
+                if (v1 < v2) return -1;
+                start1 = idx1 + 1;
+                start2 = idx2 + 1;
+            }
+        }
+        return 0;
     }
 };
